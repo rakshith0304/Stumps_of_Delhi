0e95886dcbc479a7cf1afbfc672e7446214fc48d
rakshith0304 <rakshith3040@gmail.com>
2024-11-09 12:10:37 -0700

    added new pages

---
+++ FRONTEND/src/components/Aboutus_page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aboutus_page = () => {
+  return (
+    <div>Aboutus_page</div>
+  )
+}
+
+export default Aboutus_page
---
+++ FRONTEND/src/components/shop_page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const shop_page = () => {
+  return (
+    <div>shop_page</div>
+  )
+}
+
+export default shop_page
